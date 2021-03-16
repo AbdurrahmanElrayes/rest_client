ec83afd6ced38e9cb219e031246ed03d66b83d60
Abdurrahman Elrayes <32941712+AbdurrahmanElrayes@users.noreply.github.com>
2021-03-16 10:16:18 +0200

    Update client.dart
    The argument type 'String?' can't be assigned to the parameter type 'String' because 'String?' is nullable and 'String' isn't

--- lib/src/client/client.dart
+++ lib/src/client/client.dart
@@ -155,7 +155,7 @@ class Client {
 
         dynamic responseBody;
         if (body?.isNotEmpty == true) {
-          responseBody = await processJson(body);
+          responseBody = await processJson(body)!;
         }
 
         var response = Response(
